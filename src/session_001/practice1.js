d7d278b187e9647c5bfa9fc76f2d5d9260de9b6b
Anisha Narang <anarang@redhat.com>
2017-06-12 14:29:25 +0530

    adding solution for practice 1 and 2

--- src/session_001/practice1.js
+++ src/session_001/practice1.js
@@ -1,3 +1,15 @@
-(function() {
-  // Todo
-})();
+function clock(){
+	var d = new Date();
+	var date = d.getDate();
+	var month = d.getMonth();
+	var year = d.getFullYear();
+
+	var hour = d.getHours();
+	var minute = d.getMinutes();
+	var sec = d.getSeconds();
+
+	current_date = year + "-" + month + "-" + date + " " + hour + ":" + minute + ":" + sec ;
+	
+	return current_date;
+};
+document.getElementById('practice_1_answer').innerHTML = clock();
--- src/session_001/practice2.js
+++ src/session_001/practice2.js
@@ -1,3 +1,12 @@
-(function() {
-  // Todo
-})();
+
+var num = 0
+function callme() {
+	document.getElementById('practice_2_answer').innerHTML = clock();
+	setTimeout(callme, 1000);
+}
+
+callme();
+
+// console.log(clk.cloak());
+
+// document.getElementById('practice_2_answer').innerHTML = setInterval('clk.clock()',1000);
